d02cc8992e341cdc9959151d280dced016d71165
Hossin <94375287+qwe2qwee@users.noreply.github.com>
2023-11-25 14:39:55 +0300

    backend

--- backend-p/schemas/index.js
+++ backend-p/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = [];
+import testimonials from './testimonials'
+import about from './about'
+import brand from './brand'
+import contact from './contact'
+import experiences from './experiences'
+import work from './work'
+import workExperience from './workExperience'
+import skills from './skills'
+
+export const schemaTypes = [
+  work,
+  about,
+  brand,
+  skills,
+  contact,
+  experiences,
+  testimonials,
+  workExperience,
+]
---
+++ frontend-p/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+// import { Navbar } from './components';
+// import './App.scss';
+
+const App = () => (
+  <div className="app">
+
+    app
+
+  </div>
+);
+
+export default App;
